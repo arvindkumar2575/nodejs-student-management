a1bb85e3086d752e82329ea0c06aa61ffab92cb8
Arvind Kumar <arvindkumar2575@gmail.com>
2022-08-21 10:51:40 +0530

    add public files and is working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,45 @@
 const express = require('express');
 const app = express()
-const page = require(__dirname+'/components/page.js')
+const page = require(__dirname+'/pages/page')
+
+//set public folder
+app.use(express.static('public'))
 
 
 app.get('/home',function(req,res){
-  res.send(page.index);
+  let data = {
+    meta:{
+      title:'New Page',
+      stylesheets:[
+        {
+          path:'css/style.css'
+        }
+      ]
+    },
+    body:{
+      body_class:'main_app',
+      navbar:[
+        {
+          id:1,
+          name:'Home',
+          link:'/home'
+        },
+        {
+          id:2,
+          name:'About Us',
+          link:'/about-us'
+        }
+      ],
+      scripts:[
+        {
+          path:'js/app.js',
+        }
+      ],
+      first_name:'arvind',
+      last_name:'kumar'
+    }
+  }
+  res.send(page.index(data));
 });
 
 app.listen(process.env.port || 3000);
